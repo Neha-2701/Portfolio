865d8cc8993cbc4733f5bdb53a8a11ccf3e45dc4
Neha Garg <nehagarg@Nehas-MacBook-Air.local>
2023-11-06 13:27:55 +0530

    Added responsiveness

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -75,7 +75,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-black to-gray-800 w-full h-screen sm:h-full"
+      className="bg-gradient-to-b from-black to-gray-800 w-full h-full lg:h-screen sm:h-full"
     >
       <div className=" max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full  text-white">
         <div>
